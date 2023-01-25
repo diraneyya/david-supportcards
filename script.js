17f15ccb8172d103d6fcd39bd5a56648d096df03
Orwa Diraneyya <107997002+diraneyya@users.noreply.github.com>
2023-01-25 19:53:09 +0100

    commented out Sarah's line
    Sarah @keylan1 if you get a chance to fill in this tonight please follow the instructions in the code, otherwise we can just hand over the cards tomorrow to David. Since it is an online repo, you will be able to fill in the information at any time you want and it will get updated. Cheers.

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ const supportCardData = [
             target="_blank">song</a> for you, beautiful
         `,
   },
+  /*
+  // SARAH PLEASE UNCOMMENT THIS PART BY REMOVING the forward slash asterisk in
+  // line 31 and the asterisl forward slash in line 42 and filling your message
   {
     name: "Sarah",
     emoji: "placeholder",
@@ -36,6 +39,7 @@ const supportCardData = [
             placeholder
         `,
   },
+  */
   {
     name: "Lisa",
     emoji: "💗💫",

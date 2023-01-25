995d0ca6d9a3480e0545d2863e020f111da93f94
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-01-25 12:49:40 +0100

    commit initially

---
+++ README.md
@@ -0,0 +1,19 @@
+# What is this
+
+This is a repo that I created to be able to show David our support 💞 for the death of a dog he held dear.
+
+## How do I contribute?
+
+Edit the file `script.js` in the root of the repo.
+
+In this file, you will see a part that corresponds to you, for example, check my part and what I inserted in the different portions of it.
+
+Since you do not know Javascript yet, there is a chance you might insert syntactical errors which will break the code. Do not worry I will fix these when needed.
+
+You can also clone the repo locally and test your changes before pushing them to GitHub. Just use the Live Server to view the file `index.html` in the browser.
+
+Everyone who has been invited to this repo has access and push to the `main` branch directly, to simplify things. Branching is not needed (unless you wish to change the styling, for example add more stuff and animations, feel free to).
+
+I would like to finalise this by tonight, if possible! So we can show it to him tomorrow morning!
+
+🥰🥰🥰
---
+++ script.js
@@ -0,0 +1,108 @@
+/**
+ * STUDENT PARTICIPANT
+ * PLEASE UPDATE YOUR PART HERE
+ * 
+ * Instructions:
+ *   - Go to the curly-brace-scoped part with your name inside it
+ *   - Change the emoji, this is a list of the glyphs available in
+ *     the emoji font used: 
+ *     https://fonts.google.com/noto/specimen/Noto+Emoji/glyphs
+ *   - Change the title, one or two words max
+ *   - Change the paragraph, spill your heart out
+ * 
+ * Ideas:
+ *  - The paragraph part can have links, remember, David is a musi-
+ *    cian, so by including a link to a track, you might manage to
+ *    touch his heart more easily.
+ *  - Images can also be included, in which case, try to use your
+ *    limited knowledge to ensure the image (sizing and color wise)
+ *    blends well with the rest of the page
+ */
+const supportCardData = [
+    {
+        name: "Orwa",
+        emoji: "💛❤️🧡💝",
+        title: "Much love",
+        paragraph: `
+            A <a href="https://open.spotify.com/album/77mt6Wp8IJ4arMlbwoJiNH"
+            target="_blank">song</a> for you, beautiful
+        `
+    },
+    {
+        name: "Sarah",
+        emoji: "placeholder",
+        title: "placeholder",
+        paragraph: `
+            placeholder
+        `
+    },
+    {
+        name: "Lisa",
+        emoji: "placeholder",
+        title: "placeholder",
+        paragraph: `
+            placeholder
+        `
+    },
+    {
+        name: "Yevhenii",
+        emoji: "placeholder",
+        title: "placeholder",
+        paragraph: `
+            placeholder
+        `
+    },
+    {
+        name: "Nadia",
+        emoji: "placeholder",
+        title: "placeholder",
+        paragraph: `
+            placeholder
+        `
+    },
+    {
+        name: "Alisa",
+        emoji: "placeholder",
+        title: "placeholder",
+        paragraph: `
+            placeholder
+        `
+    },
+];
+
+/**
+ * STUDENT PARTICIPANT
+ * DO NOT UPDATE THIS PART
+ */
+if ('content' in document.createElement('template')) {
+    // Grab the support-card template from the page
+    const template = document.querySelector('#supportcard');
+
+    const shuffledCardData = supportCardData.sort(
+        (a, b) => 0.5 - Math.random());
+    for (supportCard of shuffledCardData) {
+
+        // Clone the support card, modify its fields and insert 
+        // it into the document
+        const clone = template.content.cloneNode(true);
+        const fields = clone.querySelectorAll("#field");
+        fields[0].textContent = supportCard.emoji;
+        if (/<([^> ]+)[^>]+>.*<\/\1>/s.test(supportCard.paragraph))
+            fields[1].innerHTML = supportCard.paragraph;
+        else
+            fields[1].textContent = supportCard.paragraph;
+        fields[2].textContent = supportCard.title;
+    
+        document.querySelector('.container').appendChild(clone);
+    }
+} else {
+  // Find another way to add the rows to the table because
+  // the HTML template element is not supported.
+  document.body.innerHTML = `
+    <div class="container">
+        <div class="face face2">
+            <h2>Browser not supported</h2>
+        </div>
+    </div>
+  `;
+}
